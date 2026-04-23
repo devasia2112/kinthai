1862b7f6f5a9cbd67aab88bd33c2ef46e7db090c
devasia <deepcell@gmail.com>
2026-04-23 14:39:37 +0100

    assets for the chef calculator
    assets for the chef calculator

---
+++ chefcalc/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "chef-calc-v4";
+
+const urlsToCache = [
+  "./",
+  "./private_chef_cost_calculator_v4.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
